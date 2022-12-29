70a5d0103ca1ec4103b54e91fb12aa5fee4364da
yves51 <110249689+yves51@users.noreply.github.com>
2022-12-29 08:37:18 +0000

    exerciceXP day2

--- DI-Bootcamp/week2/Day2/exercisesXP/script.js
+++ DI-Bootcamp/week2/Day2/exercisesXP/script.js
@@ -25,6 +25,19 @@ if (newDogdont = newDogdont) {
     console.log(nonAimer);
 }
 
+
+////////////////////////////////////////////Exercice4//////////////////////////////////////////////////////////////////;
+const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+if (users.length==0) {
+    console.log("personne n'est en ligne");
+} else if (users.length==1) {
+    console.log(users[0] + " is online ");
+}else if (users.length ===2) {
+    console.log(users[0] && users[1] + "are online ");
+} else {
+    console.log(users[0], users[1],users[2] &&  users.length - 1 + " more online");
+}
+
 ////////////////////////////////////////////Exercice3//////////////////////////////////////////////////////////////////;
 
 let entreNombre = prompt("entrer un nombre")
@@ -39,14 +52,3 @@ console.log(entreNombre);
                      alert(entreNombre  + " est un nombre impair");
               }
 
-////////////////////////////////////////////Exercice4//////////////////////////////////////////////////////////////////;
-const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
-if (users.length==0) {
-    console.log("personne n'est en ligne");
-} else if (users.length==1) {
-    console.log(users[0] + " is online ");
-}else if (users.length ===2) {
-    console.log(users[0] && users[1] + "are online ");
-} else {
-    console.log(users[0], users[1],users[2] &&  users.length - 1 + " more online");
-}
