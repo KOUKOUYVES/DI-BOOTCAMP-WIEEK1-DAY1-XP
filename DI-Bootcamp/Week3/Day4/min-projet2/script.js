ea506b991551698c1e772829f150c913590febea
yves51 <110249689+yves51@users.noreply.github.com>
2023-01-09 20:18:13 +0000

    mini-projet2 day4

--- DI-Bootcamp/Week3/Day4/min-projet2/script.js
+++ DI-Bootcamp/Week3/Day4/min-projet2/script.js
@@ -1,11 +1,12 @@
-let toutesCle = [65,83,68,70,71,72,74,75,76];
 
+let toutesCle = [65,83,68,70,71,72,74,75,76];
 window.addEventListener('keydown', (event) =>{
     	let keyCode = event.code;
-    	if (!toutesCle.includes(keyCode)) return;
+    	if (!toutesCle.includes(keyCode))
+		return;
     	event.preventDefault();
     	event.stopPropagation();
-    	playAudio(keyCode)
+    	play(keyCode)
 });
 
 // Get all elements content class "drum"
@@ -13,11 +14,11 @@ let drums = document.getElementsByClassName("drum");
 for (let i = 0; i < drums.length; i++) {
     drums[i].addEventListener('click', function(){
     	let keyCode = this.getAttribute("data-key");
-    	playAudio(keyCode); 
+    	play(keyCode); 
     }, false);
 }
 // play audio container by key
-const playAudio = (keyCode) => { 
+const play = (keyCode) => { 
 	let note = document.querySelector("audio[data-key='"+keyCode+"']");
 	let drum = document.querySelector(".drum[data-key='"+keyCode+"']");
 	note.currentTime = 0;  //Sets current position of playback to 0. So the sound restarts if its still playing.
